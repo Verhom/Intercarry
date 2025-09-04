7b661c44ac142dae0107f609aab8cff21b087fb2
Verhom <159990092+Verhom@users.noreply.github.com>
2025-09-03 23:56:14 -0400

    Set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Importante: base = nombre del repo entre slashes
-  base: '/flujo-imports/',
+  base: '/Intercarry/', // nombre EXACTO del repo en GitHub
 })
